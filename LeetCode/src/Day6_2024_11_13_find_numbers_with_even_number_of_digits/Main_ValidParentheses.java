e451a23ca34bf0f47f117db91f80ea85d57cfaec
Toannd832 <toan03182@gmail.com>
2024-11-13 23:59:33 +0700

    Day 6

---
+++ LeetCode/src/Day6_2024_11_13_find_numbers_with_even_number_of_digits/Main_ValidParentheses.java
@@ -0,0 +1,12 @@
+package Day6_2024_11_13_find_numbers_with_even_number_of_digits;
+
+public class Main_ValidParentheses {
+
+	public static void main(String[] args) {
+
+		Solution solution = new Solution();
+		int[] num = {555,901,482,1771};
+		int result = solution.findNumbers(num);
+		System.out.println(result);
+	}
+}
---
+++ LeetCode/src/Day6_2024_11_13_find_numbers_with_even_number_of_digits/Solution.java
@@ -0,0 +1,34 @@
+/*
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+	You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+	You can return the answer in any order.
+	
+ * */
+package Day6_2024_11_13_find_numbers_with_even_number_of_digits;
+
+import java.util.Iterator;
+
+public class Solution {
+
+	public int findNumbers(int[] nums) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+
+			if (countNumbers(nums[i]) % 2 == 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int countNumbers(int n) {
+		int count = 0;
+		while (n != 0) {
+			n /= 10;
+			count++;
+		}
+		return count;
+	}
+}
